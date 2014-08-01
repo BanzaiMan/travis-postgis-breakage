0130d935a7edd8aec7178c72e7d15922ef224dd2
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-08-01 16:46:15 +0100

    make sure statement is correct

--- test.sh
+++ test.sh
@@ -3,4 +3,8 @@ psql -c 'create database test;' -U postgres
 psql -U postgres -c "create extension postgis" -d test
 cat bytea_import.sql | psql -U postgres -d test
 FILE=`readlink -f raster.tif`
-psql -U postgres -c "SELECT ST_FromGDALRaster(bytea_import('$FILE'), 4326);" -d test
+echo "$FILE"
+echo "SELECT ST_FromGDALRaster(bytea_import('$FILE'), 4326);"
+STATEMENT="SELECT ST_FromGDALRaster(bytea_import('$FILE'), 4326);"
+echo "$STATEMENT"
+psql -U postgres -c "$STATEMENT" -d test
