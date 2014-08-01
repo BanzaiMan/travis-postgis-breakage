db5645078921bd9c079ff322850a39ef6737b44d
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-08-01 16:48:04 +0100

    trim

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ psql -U postgres -c "create extension postgis" -d test
 cat bytea_import.sql | psql -U postgres -d test
 FILE=`readlink -f raster.tif`
 echo "$FILE"
-echo "SELECT ST_FromGDALRaster(bytea_import('$FILE'), 4326);"
 STATEMENT="SELECT ST_FromGDALRaster(bytea_import('$FILE'), 4326);"
 echo "$STATEMENT"
 psql -U postgres -c "$STATEMENT" -d test
