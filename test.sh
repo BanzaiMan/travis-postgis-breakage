81de79d1e0b6857e80381d73c36f5decc7db405d
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-08-01 16:39:07 +0100

    Fix bash stuff

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 psql -c 'create database test;' -U postgres
 psql -U postgres -c "create extension postgis" -d test
 cat bytea_import.sql | psql -U postgres -d test
-FILE=${readlink -f raster.tif}
+FILE=`readlink -f raster.tif`
 psql -U postgres -c "SELECT ST_FromGDALRaster(bytea_import('$FILE'), 4326);" -d test
