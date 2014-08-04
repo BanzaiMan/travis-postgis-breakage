ac6633fe3df8db0cc83f5f467bd3147cc46b380c
Laurence Hudson <Laurence.Hudson@tessella.com>
2014-08-04 10:12:22 +0100

    Run better on travis test VMs

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-psql -c 'create database test;' -U postgres
-psql -U postgres -c "create extension postgis" -d test
-cat bytea_import.sql | psql -U postgres -d test
+psql -U postgres -c "drop database if exists test;"
+psql -U postgres -c 'create database test;'
+psql -U postgres -d test -c "create extension postgis"
+psql -U postgres -d test -f bytea_import.sql
 FILE=`readlink -f raster.tif`
 echo "$FILE"
 STATEMENT="SELECT ST_FromGDALRaster(bytea_import('$FILE'), 4326);"
 echo "$STATEMENT"
-psql -U postgres -c "$STATEMENT" -d test
+psql -U postgres -d test -c "$STATEMENT"
+EXIT=$?
+echo "EXIT CODE: $EXIT"
+exit $EXIT
